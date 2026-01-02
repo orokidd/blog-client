69bf03f729d1242faae85cdd4a0c2a950d401779
orokidd <gregomily@gmail.com>
2026-01-02 16:04:45 +0800

    home page: fetch posts from API

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,6 +1,30 @@
-import { posts } from "../assets/mockPost";
+import { useEffect, useState } from "react";
+// import { posts } from "../assets/mockPost";
 
 export function PostList() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+
+    async function fetchPosts() {
+      try {
+        const res = await fetch("http://localhost:3000/api/posts");
+        if (!res.ok) throw new Error("Failed to fetch data");
+
+        const data = await res.json();
+        setPosts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchPosts();
+  }, []);
+
+  if (posts.length === 0) {
+    return <div>No posts found</div>;
+  }
+
   return (
     <div className="posts-container">
       {posts.map((element, index) => (
@@ -10,5 +34,5 @@ export function PostList() {
         </div>
       ))}
     </div>
-  )
+  );
 }

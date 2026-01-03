1070c9af577de8eb1cd3e2f47ca53c9c5f22b0d0
orokidd <gregomily@gmail.com>
2026-01-03 08:38:54 +0800

    fetch post comments from API

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,14 +1,28 @@
-export function CommentList({ postId }) {
-  const comments = [
-    {
-      content: "Comment test arasaka",
-      id: 1,
-    },
-    {
-      content: "Comment test bisoprolol",
-      id: 2,
-    },
-  ];
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
+export function CommentList() {
+  const {postId} = useParams()
+  const [comments, setComments] = useState([])
+
+  useEffect(() => {
+
+    async function fetchComments() {
+      try {
+        const res = await fetch(`http://localhost:3000/api/comments/post/${postId}`)
+        if (!res.ok) throw new Error("Failed to fetch data");
+
+        const data = await res.json();
+        setComments(data);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchComments()
+  }, [postId])
+
+  console.log(comments)
 
   return (
     <div className="comments-container">
@@ -16,6 +30,7 @@ export function CommentList({ postId }) {
       {comments.map((comment) => {
         return (
           <div className="comment" key={comment.id}>
+            <div className="comment-username">{comment.user.username}</div>
             <div className="comment-content">{comment.content}</div>
           </div>
         );

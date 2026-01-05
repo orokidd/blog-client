f6866e489998771c8a791038a2118edd360d4477
orokidd <gregomily@gmail.com>
2026-01-05 12:43:21 +0800

    Handle delete comment: post new comment to API then update the comment list state to trigger re-render

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -8,7 +8,7 @@ export function Comments() {
   const { postId } = useParams()
   const [comments, setComments] = useState([])
   const [loading, setLoading] = useState(true)
-  const { loggedIn, user } = useContext(AuthContext)
+  const { loggedIn, user, getToken } = useContext(AuthContext)
 
   const fetchComments = async () => {
     setLoading(true)
@@ -22,14 +22,36 @@ export function Comments() {
     setLoading(false)
   }
 
+  const handleDeleteComment = async (commentId) => {
+    try {
+      const res = await fetch(`http://localhost:3000/api/comments/${commentId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${getToken()}`
+        }
+      })
+
+      if (res.ok ) {
+        const data = await res.json()
+        console.log(data.message)
+        setComments(prevComments => prevComments.filter(comment => comment.id !== commentId))
+      } else {
+        console.error('Failed to delete comment');
+      }
+    } catch (error) {
+        console.error('Error deleting comment:', error);
+    }
+  }
+
   useEffect(() => {
     fetchComments()
   }, [postId])
 
   return (
     <>
-      <CommentForm loggedIn={loggedIn} postId={postId} onCommentCreated={fetchComments} />
-      <CommentList comments={comments} loading={loading} user={user} />
+      <CommentForm loggedIn={loggedIn} postId={postId} onCommentCreated={fetchComments} getToken={getToken} />
+      <CommentList comments={comments} loading={loading} user={user} onDeleteComment={handleDeleteComment}/>
     </>
   )
 }

f43d6559eb7aaf6202a43a45061ab4639891e299
orokidd <gregomily@gmail.com>
2026-01-05 11:44:35 +0800

    Added route to register page

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import Post from "../pages/Post";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 // export const router = createBrowserRouter([
 //   {
@@ -23,6 +24,10 @@ export const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
   {
     path: "/post/:postId",

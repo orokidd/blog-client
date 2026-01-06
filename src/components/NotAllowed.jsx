30b5925de42faf94678c45f801a1da9e11bf3e04
orokidd <gregomily@gmail.com>
2026-01-06 13:00:17 +0800

    Restricted page and not found page

---
+++ src/components/NotAllowed.jsx
@@ -0,0 +1,7 @@
+export default function NotAllowed() {
+    return (
+        <div className="not-allowed">
+            <h1>You are not allowed to access this page.</h1>
+        </div>
+    )
+}
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div className="not-found-page">
+            <h1>Page not found.</h1>
+        </div>
+    )
+}

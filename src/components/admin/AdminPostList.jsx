89062344b77a5fa59328d802d5e971d9953c2f25
orokidd <gregomily@gmail.com>
2026-01-10 11:33:55 +0800

    Refactor admin dashboard: move state up to its parent component

--- src/components/admin/AdminPostList.jsx
+++ src/components/admin/AdminPostList.jsx
@@ -2,51 +2,17 @@ import {useContext, useEffect, useState} from "react";
 import {AuthContext} from "../../context/AuthContext";
 import {Link} from "react-router-dom";
 
-export default function AdminPostList() {
-    const [posts, setPosts] = useState([]);
-    const [error, setError] = useState(null);
-    const { getToken } = useContext(AuthContext);
-
-    async function fetchPosts() {
-        try {
-            const res = await fetch('http://localhost:3000/api/posts');
-            if (!res.ok) throw new Error("Failed to fetch data");
-
-            const data = await res.json();
-            setPosts(data);
-        } catch (e) {
-            setError(e.message);
-            console.log(e.message)
-        }
+export default function AdminPostList({posts, deletePost, error, loading}) {
+    if (loading) {
+        return <div>Loading...</div>;
     }
-
-    async function deletePost(postId) {
-        try {
-            const res = await fetch(`http://localhost:3000/api/posts/${postId}`, {
-                method: "DELETE",
-                headers: {
-                    "content-type": "application/json",
-                    "Authorization": `Bearer ${getToken()}`
-                }
-            });
-
-            if (!res.ok) throw new Error("Failed to delete post");
-
-            const data = await res.json();
-            console.log("Post deleted successfully:", data);
-            await fetchPosts();
-        } catch (e) {
-            setError(e.message);
-            console.log(e.message)
-        }
-    }
-
-    useEffect(() => {
-        fetchPosts();
-    }, []);
 
     if (posts.length === 0) {
-        return <div>{error}</div>;
+        return <div>No posts found.</div>;
+    }
+
+    if (error) {
+        return <div className="error">Error: {error}</div>;
     }
 
     return (
--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,10 +1,56 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../context/AuthContext.jsx";
 import AdminPostList from "../../components/admin/AdminPostList.jsx";
 import ProtectedPage from "../ProtectedPage.jsx";
 
 export default function AdminDashboard() {
+    const [posts, setPosts] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const { getToken } = useContext(AuthContext);
+
+    async function fetchPosts() {
+        try {
+            const res = await fetch('http://localhost:3000/api/posts');
+            if (!res.ok) throw new Error("Failed to fetch data");
+
+            const data = await res.json();
+            setPosts(data);
+            setLoading(false);
+        } catch (e) {
+            setError(e.message);
+            console.log(e.message)
+        }
+    }
+
+    async function deletePost(postId) {
+        try {
+            const res = await fetch(`http://localhost:3000/api/posts/${postId}`, {
+                method: "DELETE",
+                headers: {
+                    "content-type": "application/json",
+                    "Authorization": `Bearer ${getToken()}`
+                }
+            });
+
+            if (!res.ok) throw new Error("Failed to delete post");
+
+            const data = await res.json();
+            console.log("Post deleted successfully:", data);
+            setPosts(posts.filter(post => post.id !== postId));
+            // await fetchPosts();
+        } catch (e) {
+            setError(e.message);
+            console.log(e.message)
+        }
+    }
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
     return (
         <ProtectedPage>
-            <AdminPostList />
+            <AdminPostList posts={posts} deletePost={deletePost} error={error} loading={loading}/>
         </ProtectedPage>
     )
 }
